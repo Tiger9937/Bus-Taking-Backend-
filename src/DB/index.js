6bfd034f74be53b2a130396d738af3181687f2cc
jagannath sahoo <100899474+Tiger9937@users.noreply.github.com>
2024-08-19 06:13:17 -0700

    connect data base i usse mongoDB for this project . this project is use the mongodb atlas for DB conaction

---
+++ src/DB/index.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import {D_BName} from '../constants.js';
+
+const connectDB = async ()=>{
+    try {
+      await mongoose.connect(`${process.env.MONGODBURI} / ${D_BName}`)
+      console.log(" mongodb husband connected ")
+    } catch (error) {
+        console.log("Momngodb Conaction error",error)
+        process.exit(1)
+    }
+}   
+
+export default connectDB
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const D_BName = "BUS ROOT"
