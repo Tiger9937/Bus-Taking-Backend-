594647a57e79bb1aa4408604c45199957dbdaf5a
jagannath sahoo <100899474+Tiger9937@users.noreply.github.com>
2024-08-21 08:12:17 -0700

    we install 3 new package and bulid a file upload functionality package1->cloudinary  Is helping To stored user files package2->multer  is helping to upload file in user system to our server package3->express-fileupload is helping to upload file in user system to our server
    (express-fileupload and multer did the same thing but for the testing prepuse i just install this two pakage 😎😎😎😎😎😎😎😎)
    
    -->bulid a file upload<--
    To bulid a file upload i use two file multer(middlware) , cloudinary(utility)

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+
+const cloudinary_FUNCTION = async (Local_File_Path)=>{
+    try {
+        if(!Local_File_Path){return null}
+        // Configuration
+        cloudinary.config({ 
+            cloud_name: `${process.env.CLOUD_NAME}`, 
+            api_key: `${process.env.API_KEY}`, 
+            api_secret: `${process.env.API_SECRET}` 
+        });
+        //Upload an file
+                const fileRSPOS = await cloudinary.uploader.upload(Local_File_Path,{
+                resource_type: 'auto'
+            }) 
+            console.log("file has been successfully uploaded",fileRSPOS.url)
+            return fileRSPOS
+    } catch (error) {
+            fs.unlink(Local_File_Path)
+            return null
+    }
+
+} 
+export{cloudinary_FUNCTION}
